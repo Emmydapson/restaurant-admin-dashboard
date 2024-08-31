22e4004a3e24cdeb6b4279c14dadd2e648e1c256
emmydapson <iloriemmanuel00@gmail.com>
2024-08-31 22:48:11 +0100

    error fixed

--- src/app/hooks/useAuth.js
+++ src/app/hooks/useAuth.js
@@ -4,35 +4,36 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 const useAuth = () => {
-  const [loading, setLoading] = useState(true);
-  const [authenticated, setAuthenticated] = useState(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const response = await fetch('https://look-my-app.vercel.app/api/auth/verify-token', {
-          method: 'GET',
-          credentials: 'include', // Ensure cookies are sent with the request
-        });
-
-        if (response.ok) {
-          setAuthenticated(true);
-        } else {
+    const [loading, setLoading] = useState(true);
+    const [authenticated, setAuthenticated] = useState(false);
+    const router = useRouter();
+  
+    useEffect(() => {
+      const checkAuth = async () => {
+        try {
+          const response = await fetch('https://look-my-app.vercel.app/api/auth/verify-token', {
+            method: 'POST',  // Changed to POST
+            credentials: 'include', // Ensure cookies are sent with the request
+          });
+  
+          if (response.ok) {
+            setAuthenticated(true);
+          } else {
+            router.push('/login');
+          }
+        } catch (error) {
+          console.error('Authentication check failed:', error);
           router.push('/login');
+        } finally {
+          setLoading(false);
         }
-      } catch (error) {
-        console.error('Authentication check failed:', error);
-        router.push('/login');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    checkAuth();
-  }, [router]);
-
-  return { loading, authenticated };
-};
-
-export default useAuth;
+      };
+  
+      checkAuth();
+    }, [router]);
+  
+    return { loading, authenticated };
+  };
+  
+  export default useAuth;
+  
